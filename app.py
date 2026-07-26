530f27596fba0a57486ecac6a087a04039e4d17c
hastisf <hastisfatmawati@gmail.com>
2026-07-27 01:17:07 +0700

    app.py

--- app.py
+++ app.py
@@ -124,7 +124,7 @@ section[data-testid="stSidebar"] details summary:hover{
     padding:18px;
     text-align:center;
     box-shadow:0 2px 6px rgba(0,0,0,.06);
-    height:125px;
+    height:130px;
 }
 
 .metric-icon{
